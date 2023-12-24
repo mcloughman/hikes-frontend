44e7b7c347793fa8f294adae7e392a6f9a428402
Michael Loughman <mcloughman@gmail.com>
2023-12-24 15:01:00 -0800

    Implement loading state. The goal being to eliminiate the choppy rendering of the images. Better to display a loading message and wait for all elements to be on the page for better UX.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useHikesContext } from "../hooks/useHikesContext"
 
 // components
@@ -6,24 +6,39 @@ import Hike from "../components/Hike"
 
 const Home = () => {
   const { hikes, dispatch } = useHikesContext()
+  const [loading, setLoading] = useState(false)
+
   useEffect(() => {
+    setLoading(true)
     const fetchHikes = async () => {
-      const response = await fetch(
-        "https://hikes-backend-e698c568813b.herokuapp.com/api/hikes"
-      )
-      const hikes = await response.json()
-      if (response.ok) {
-        dispatch({ type: "SET_HIKES", payload: hikes })
+      try {
+        const response = await fetch(
+          "https://hikes-backend-e698c568813b.herokuapp.com/api/hikes"
+        )
+        const hikes = await response.json()
+        if (response.ok) {
+          setLoading(false)
+          dispatch({ type: "SET_HIKES", payload: hikes })
+        }
+      } catch (e) {
+        console.log(e)
+        setLoading(false)
       }
     }
     fetchHikes()
   }, [dispatch])
   return (
-    <div className="home">
-      <div className="hikes">
-        {hikes && hikes.map((hike) => <Hike key={hike._id} hike={hike} />)}
-      </div>
-    </div>
+    <>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div className="home">
+          <div className="hikes">
+            {hikes && hikes.map((hike) => <Hike key={hike._id} hike={hike} />)}
+          </div>
+        </div>
+      )}
+    </>
   )
 }
 export default Home
